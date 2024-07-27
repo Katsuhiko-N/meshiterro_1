58894c3dc71bc1ac9676776b428e7a140833387e
Katsuhiko-N <173056385+Katsuhiko-N@users.noreply.github.com>
2024-07-27 13:45:18 +0000

    [Finish]4-5

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  # ルーティング自動追加
   root to: 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
