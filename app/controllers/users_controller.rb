8c7dcc166ea2616dfddeb14c32610155d193e4fa
Katsuhiko-N <173056385+Katsuhiko-N@users.noreply.github.com>
2024-07-28 10:06:26 +0000

    [Finish/Add]4-25/アクセス制限実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+    
+    before_action :is_matching_login_user, only: [:edit, :update]
+    
     def show
         @user = User.find(params[:id])
         @post_images = @user.post_images.page(params[:page])
@@ -10,7 +13,6 @@ class UsersController < ApplicationController
     
     def update
         @user = User.find(params[:id])
-        @user.update(user_params)
         # ログインしたユーザーのidページ
         redirect_to user_path(current_user.id)
     end
@@ -21,6 +23,15 @@ class UsersController < ApplicationController
     def user_params
         params.require(:user).permit(:name, :profile_image)
     end
+    
+    def is_matching_login_user
+        user = User.find(params[:id])
+        unless user.id == current_user.id
+            redirect_to post_images_path
+        end
+    end
+        
+    
     
     
 end
