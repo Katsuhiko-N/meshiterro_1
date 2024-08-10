50e822faec012797382af36d30f1a5692bbd6003
Katsuhiko-N <173056385+Katsuhiko-N@users.noreply.github.com>
2024-08-10 13:49:49 +0000

    ユーザ削除画面作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
-  # devise_for :admins
   devise_for :users
-  
-  devise_for :admin, skip:[:registrations, :password], controllers:{
-    sessions: 'admin/sessions'
-  }
   
   # ルーティング自動追加
   root to: 'homes#top'
@@ -19,19 +14,14 @@ Rails.application.routes.draw do
   resources :users, only: [:show, :edit, :update]
   
   # admin用ルーティング
-  device_for :admin, skip: [:registrations, :password], controllers: {
+  devise_for :admin, skip: [:registrations, :password], controllers: {
     sessions: 'admin/sessions'
   }
   
   namespace :admin do
     get 'dashbords', to: 'dashboards#index'
+    resources :users, only: [:destroy]
   end
-  
-  
-  
-  
-  
-  
   
   
   
