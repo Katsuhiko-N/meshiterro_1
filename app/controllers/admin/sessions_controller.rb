1b6e76a76efd2692c38870145b29a5a773c5bf01
Katsuhiko-N <173056385+Katsuhiko-N@users.noreply.github.com>
2024-08-10 12:18:29 +0000

    adminログイン機能作成

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -18,7 +18,17 @@ class Admin::SessionsController < Devise::SessionsController
   #   super
   # end
 
-  # protected
+  protected
+  
+  def after_sign_in_path_for(resource)
+    admin_dashboards_path
+    # ログイン後にリダイレクトするパス
+  end
+
+  def after_sign_out_path_for(resource)
+    new_admin_session_path
+    # ログアウト後にリダイレクトするパス
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
