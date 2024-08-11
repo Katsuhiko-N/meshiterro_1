495729fff70c3a6261632fee9c8706a34dbf6120
Katsuhiko-N <173056385+Katsuhiko-N@users.noreply.github.com>
2024-08-11 08:34:58 +0000

    Public名前空間作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  
-  devise_for :users
-  
-  # ルーティング自動追加
-  root to: 'homes#top'
-  get 'homes/about' => 'homes#about', as: "about"
-  
-  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
-    resource :favorite, only: [:create, :destroy]
-    resources :post_comments, only: [:create, :destroy]
+  scope module: :public do
+    devise_for :users
+    # ルーティング自動追加
+    root to: 'homes#top'
+    get 'homes/about' => 'homes#about', as: "about"
+    
+    resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+      resource :favorite, only: [:create, :destroy]
+      resources :post_comments, only: [:create, :destroy]
+    end
+    resources :users, only: [:show, :edit, :update]
   end
-  resources :users, only: [:show, :edit, :update]
   
   # admin用ルーティング
   devise_for :admin, skip: [:registrations, :password], controllers: {
