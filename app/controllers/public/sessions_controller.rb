c473a105e657f553712c5a56f89f04497daf2a15
Katsuhiko-N <173056385+Katsuhiko-N@users.noreply.github.com>
2024-08-12 15:23:25 +0000

    ログイン機能再修正-email対応

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -2,6 +2,7 @@
 
 class Public::SessionsController < Devise::SessionsController
   before_action :configure_permitted_parameters, if: :devise_controller?
+
   # before_action :configure_sign_in_params, only: [:create]
   
   
@@ -30,10 +31,10 @@ class Public::SessionsController < Devise::SessionsController
   # end
 
   protected
-    
-    def configure_permitted_parameters
-        devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-    end
+ 
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
